481dc13d519639fc5ac57bf272003bbb07aad9a8
yashkatore31 <ybkatore31@gmail.com>
2025-05-26 16:07:19 +0530

    resolved eslint issues

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -55,17 +55,16 @@ export const PinContainer = ({
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
       </div>
-      <PinPerspective title={title} href={href} />
+      <PinPerspective title={title} />
     </a>
   );
 };
 
 export const PinPerspective = ({
   title,
-  href,
 }: {
   title?: string;
-  href?: string;
+
 }) => {
   return (
     <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
